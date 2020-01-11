22f9eecbdaae1e505c66b177ac88d4e1c3c25a37
ljavanbeek <info@lucasvanbeek.nl>
2020-01-11 18:51:30 +0100

    Fk safari

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -8,7 +8,6 @@ module.exports = {
     ['link', {rel: 'icon', type: 'image/png', sizes: '194x194', href: '/favicon-194x194.png'}],
     ['link', {rel: 'icon', type: 'image/png', sizes: '192x192', href: '/android-chrome-192x192.png'}],
     ['link', {rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png'}],
-    ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#5a6b89'}],
     ['link', {rel: 'manifest', href: '/site.webmanifest'}],
     ['link', {rel: 'manifest', href: '/manifest.json'}],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes'}],
